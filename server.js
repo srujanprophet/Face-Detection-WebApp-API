a47340a93983821a8d59e6798eca690db5793c2b
srujan <srujanprophet@gmail.com>
2018-10-16 19:47:33 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image')
+
+const db = require('knex')({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    user : 'sruji',
+    password : '',
+    database : 'cerebro-faciem'
+  }
+}); // Just initializaing knex
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+
+app.get('/', (req,res) => {res.send(database.users)})
+app.get('/profile/:id',(req,res) => {profile.handleProfileGet(req,res,db)});
+app.post('/register',(req,res) => {register.handleRegister(req, res, db, bcrypt)});
+app.post('/signin', signin.handleSignin(db, bcrypt));
+app.put('/image',(req,res) => {image.handleImage(req,res,db)});
+app.post('/imageurl',(req,res) => {image.handleApiCall(req,res)});
+
+
+app.listen(3000, () => {
+	console.log('app running on port 3000');
+})
