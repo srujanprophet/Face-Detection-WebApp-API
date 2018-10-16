6b3c7d7dbe29615eedfd575b39e68b4ec9499653
srujan <srujanprophet@gmail.com>
2018-10-16 19:53:32 +0530

    Add port and fix server error

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(bodyParser.json());
 app.use(cors());
 
 
-app.get('/', (req,res) => {res.send(database.users)})
+app.get('/', (req,res) => {res.send('it is working')})
 app.get('/profile/:id',(req,res) => {profile.handleProfileGet(req,res,db)});
 app.post('/register',(req,res) => {register.handleRegister(req, res, db, bcrypt)});
 app.post('/signin', signin.handleSignin(db, bcrypt));
@@ -31,6 +31,6 @@ app.put('/image',(req,res) => {image.handleImage(req,res,db)});
 app.post('/imageurl',(req,res) => {image.handleApiCall(req,res)});
 
 
-app.listen(3000, () => {
-	console.log('app running on port 3000');
+app.listen(process.env.PORT || 3000 , () => {
+	console.log(`app running on port ${process.env.PORT)`);
 })
